3ac6eb5064a5330ff82f5a676080e15597332873
95650 <95650@DESKTOP-G6DQ5IJ>
2019-12-08 08:26:34 +0800

    write  singleModel

---
+++ CSHARP_11.21/Single.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace CSHARP_11._21
+{
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    class Single
+    {
+        private static  Single uniqueInstance;
+        private Single()
+        {
+
+        }
+
+        public static Single SetInstance()
+        {
+            if (uniqueInstance==null)
+            {
+                uniqueInstance = new Single();
+            }
+            return uniqueInstance;
+        }
+    }
+}
