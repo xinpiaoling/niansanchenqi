75b139bcaccd2a0c9a270f9c7949645973c77eb8
95650 <95650@DESKTOP-G6DQ5IJ>
2019-12-17 21:06:42 +0800

    finish linq method homework resubmit

--- CSharpLearn/ExerciseOfLinq.cs
+++ CSharpLearn/ExerciseOfLinq.cs
@@ -162,7 +162,7 @@ namespace CSharpLearn
                                          select a).First();
             Console.WriteLine(ArticleByMostComments.Title);
         }
-
+        //为求助（Problem）添加悬赏（Reward）属性，并找出每一篇求助的悬赏都大于5个帮帮币的文章作者
 
     }
 }
