0274d31bd0e941cec35cd7abbcd8341c0eb9e5e5
95650 <95650@DESKTOP-G6DQ5IJ>
2019-12-22 18:17:10 +0800

    finish GetCount method  and Test

--- CSharpLearn/StringMethod.cs
+++ CSharpLearn/StringMethod.cs
@@ -32,6 +32,29 @@ namespace CSharpLearn
             Console.WriteLine(_result);
             return _result;
         }
-
+        //实现GetCount(string container, string target)方法，
+        //可以统计出container中有多少个target
+        public static int GetCount(string container,string target)
+        {
+            if (string.IsNullOrEmpty(container) || string.IsNullOrEmpty(target))
+            {
+                throw new Exception("container and target  is not allow  Null or Empty");
+            }
+            if (container.Length<target.Length)
+            {
+                throw new Exception("contain length is not less than target length ");
+            }
+            char[] containerChar = container.ToCharArray();
+            int count = 0;
+            for (int i = 0; i <= containerChar.Length - target.Length; i++)
+            {
+                string containerSub = container.Substring(i, target.Length);
+                if (containerSub.Contains(target))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
---
+++ CSharpTest/GetCountTest.cs
@@ -0,0 +1,26 @@
+﻿using CSharpLearn;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTest
+{
+    class GetCountTest
+    {
+        [Test]
+        public void GetCountMethod()
+        {
+            Assert.AreEqual(StringMethod.GetCount("abcabcab", "ab"), 3);
+            Assert.Throws<Exception>(() => StringMethod.GetCount("abc", "abccc"));
+            Assert.Throws<Exception>(()=>StringMethod.GetCount("",""));
+            Assert.Throws<Exception>(() => StringMethod.GetCount("abc", ""));
+            Assert.Throws<Exception>(()=>StringMethod.GetCount(null,null));
+        }
+
+
+
+
+
+    }
+}
